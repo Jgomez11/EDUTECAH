6181f8461619e58b58752af9a2447c74498d28ba
Jgomez11 <37757272+Jgomez11@users.noreply.github.com>
2019-07-30 03:11:38 -0600

    act 3

--- Contenido/indexCuerpo.php
+++ Contenido/indexCuerpo.php
@@ -1,27 +1,80 @@
 <div class="ui container mt-4">
-	<?php 
-		for ($i=0; $i < 2; $i++) { 
-			echo '<div class="row mt-3">';
-			for ($j=0; $j < 4; $j++) {
-				echo '	<div class="col-md-3">
+			<div class="row mt-12">
+				
+				<div class="col-md-3">
 							<div class="ui link card">
 		  						<div class="content">
-    								<!--<div class="center aligned floated author">
-     								<img class="ui avatar image" src="">
-    								</div>-->
-    								<div class="ui center aligned header">Modulo '.$i.$j.'</div>
+    								<div class="center aligned floated author">
+     								<img class="ui medium image" src="recursos/imagenes/Modulos/M_Recursos.jpg">
+    								</div>
+    								<div class="ui center aligned header">Recursos</div>
     								<div class="meta">
       									<span class="category">Resumen</span>
     								</div>
     								<div class="description">
-      									<p>Pendiente.</p>
+      									<p>En este espacio podras encontras los recursos subido por tu profesor de clases, ademas podras filtrar la busqueda para que encuentres rapido lo que necesitas. <br> <br></p>
     								</div>
   								</div>
 							</div>
-						</div>';
-			}
-			echo '</div>';
-		}
-	 ?>
+						</div>
+
+						<div class="col-md-3">
+							<div class="ui link card">
+		  						<div class="content">
+    								<div class="center aligned floated author">
+     								<img class="ui medium image" width="450px" src="recursos/imagenes/Modulos/M_Casillero.jpg">
+    								</div>
+    								<div class="ui center aligned header">Casillero</div>
+    								<div class="meta">
+      									<span class="category">Resumen</span>
+    								</div>
+    								<div class="description">
+      									<p>En este espacio puedes guardar tus libros favoritos, y volver a ellos de una forma facil y rapida. <br> <br> <br> <br></p>
+    								</div>
+  								</div>
+							</div>
+				</div>
+			
+
+			
+				<div class="col-md-3">
+							<div class="ui link card">
+		  						<div class="content">
+    								<div class="center aligned floated author">
+     								<img class="ui medium image" src="recursos/imagenes/Modulos/M_Inventario.jpg">
+    								</div>
+    								<div class="ui center aligned header">Inventario</div>
+    								<div class="meta">
+      									<span class="category">Resumen</span>
+    								</div>
+    								<div class="description">
+      									<p>En este espacio podras editar el contenido subido por tu autoria , eliminar documentos o agregar nuevos. <br> <br> <br> </p>
+    								</div>
+  								</div>
+							</div>
+				</div>
+
+				<div class="col-md-3">
+							<div class="ui link card">
+		  						<div class="content">
+    								<div class="center aligned floated author">
+     								<img class="ui medium image" src="recursos/imagenes/Modulos/M_Soporte.jpg">
+    								</div>
+    								<div class="ui center aligned header">Soporte</div>
+    								<div class="meta">
+      									<span class="category">Resumen</span>
+    								</div>
+    								<div class="description">
+      									<p>En este espacio puedes hacer tus consultas con respecto al uso de la plataforma, ademas encontraras videos y preguntas frecuentes que podran dar una respuesta rapida a tu consulta.</p>
+    								</div>
+  								</div>
+							</div>
+				</div>
+
+
+			</div>
+			</div>
+		
+	 
 </div> 
  <p class="mt-5 mb-3 text-muted text-center">&copy; 2019</p>
